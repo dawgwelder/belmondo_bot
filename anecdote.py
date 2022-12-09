409261592a54d07119385ea5e3ee5399e47413f2
Nikolay Puchnin <nikolay.puchnin@lamoda.ru>
2022-12-09 11:50:34 +0300

    -  b anecs

--- anecdote.py
+++ anecdote.py
@@ -2,10 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 
 
-anecdote_site = "https://www.anekdot.ru/random/anekdot/"
+anecdote_site = "https://baneks.ru/random"
 
 
 def get_anecdote():
     soup = BeautifulSoup(requests.get(anecdote_site).text, features="lxml")
-    text = soup.find("div", {"class": "text"}).text
+    text = soup.find("title").text.split(" |")[0]
     return text
